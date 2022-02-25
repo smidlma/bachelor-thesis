a696c9d02703bc70314be38ef52f7ced96c06bb4
Martin Smidl <martin.smidl@struers.dk>
2022-02-25 16:09:19 +0100

    add strong type to methods

--- etl-backend/etl/schema/sources.py
+++ etl-backend/etl/schema/sources.py
@@ -1,8 +1,12 @@
-from audioop import getsample
 from os.path import exists
+
+from numpy import number
 import config
 import pandas as pd
 from pandas.io.json import build_table_schema
+import psycopg2
+from sqlalchemy import create_engine
+
 
 class Source:
     def __init__(self, name) -> None:
@@ -15,6 +19,7 @@ class Source:
     def getSample(self) -> pd.DataFrame:
         pass
 
+
 class Connection:
     def __init__(self, host, port, user, password, database) -> None:
         self.con = None
@@ -23,33 +28,36 @@ class Connection:
         self.user = user
         self.password = password
         self.database = database
-
+    
     def connect(self):
         pass
 
+    def close(self):
+        if self.con is not None:
+            self.con.close()
+
     def is_connected(self) -> bool:
+        self.connect()
         if self.con is None:
             return False
         else:
             return True
 
+
 class PostgreSQLConnection(Connection):
-    def __init__(self, host, port, user, password, database) -> None:
+    def __init__(self, host:str, port:number, user:str, password:str, database:str) -> None:
         super().__init__(host, port, user, password, database)
 
     def connect(self):
-        engine = create_engine(f'postgresql+psycopg2://test:@127.0.0.1', pool_recycle=3600)
+        engine = create_engine(f'postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}')
         try:
             self.con = engine.connect()
         except:
             self.con = None
         
 
-
-    
-
 class CSV(Source):
-    def __init__(self, name, fileName) -> None:
+    def __init__(self, name:str, fileName:str) -> None:
         self.fileName = fileName
         self.filePath = f'{config.FILE_STORAGE_PATH}{self.fileName}'
         self.defaultSchema = build_table_schema(self.getSample())
@@ -61,13 +69,15 @@ class CSV(Source):
     def getSample(self) -> pd.DataFrame:
         return pd.read_csv(self, nrows=1)
 
+
 class PostgreSQL(Source):
-    def __init__(self, name, table) -> None:
-        self.table = table
-        self.connection = Connection()
+    def __init__(self, name:str, tableName:str, connection:Connection) -> None:
+        self.tableName = tableName
+        self.connection = connection
         super().__init__(name)
 
-    def testConnection(self):
+    def testConnection(self) -> bool:
         return self.connection.is_connected()
 
-    
+    def getSample(self) -> pd.DataFrame:
+        return super().getSample()
