6d0fc397994c9a1c7ea1c6e24abf9d6c2343fee9
Martin Šmídl <snidl.martin1@gmail.com>
2022-03-02 20:33:05 +0100

    add transformation template

--- etl-backend/etl/models/transformations.py
+++ etl-backend/etl/models/transformations.py
@@ -1,5 +1,36 @@
 import mongoengine as mongo
+import pandas as pd
 
 
 class Transformation(mongo.Document):
+    name = mongo.StringField()
+    possition = mongo.IntField()
+
+    def __init__(self, *args, **values):
+        super().__init__(*args, **values)
+
+    def transform(self) -> pd.DataFrame:
+        pass
+
+
+class Sort(Transformation):
+    columns = mongo.ListField(mongo.StringField())
+    ascending = mongo.BooleanField(default=False)
+
+    def __init__(self, *args, **values):
+        super().__init__(*args, **values)
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.sort_values(by=self.columns, ascending=self.ascending)
+
+
+class Remove(Transformation):
+    pass
+
+
+class Filter(Transformation):
+    pass
+
+
+class RemoveDuplicates(Transformation):
     pass
