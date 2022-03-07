a112ab2e45a2f70cc97d32854b1118b477c77df4
Martin Šmídl <snidl.martin1@gmail.com>
2022-03-07 19:35:28 +0100

    add tests

--- etl-backend/etl/test.py
+++ etl-backend/etl/test.py
@@ -1,44 +1,24 @@
-import logging as log
 import pandas as pd
-from pandas.io.json import build_table_schema
-import psycopg2
-from sqlalchemy import create_engine, table
-from models import *
-import mongoengine as mongo
+
+
 
 
 def main():
-    pass
-    # connect to mongo
-    # mongo.connect('mongo-etl')
-    # init loger
-    # log.basicConfig(format='%(asctime)s - %(message)s', level=log.NOTSET)
+    # df = pd.read_csv('file-storage/PatientCorePopulatedTable.txt', sep='\t')
+    # df2 = pd.read_csv('file-storage/LabsCorePopulatedTable.txt', sep='\t')
 
-    # connection = PostgreSQLConnection(
-    #     host='localhost', port=5432, user='smidlma', password='', database='warehouse')
-    # CSV
-    # csvSource = CSV(sourceName='Source one', fileName='mock.csv').save()
-    # c = Source.objects().first()
-    # print(c.testConnection(), c.filePath)
+    
+    # print(df)
+    # print(df2)
 
-    # postSource = PostgreSQL(sourceName='Postgres',
-    #                         tableName='import', connection=connection)
+    # # res = df.join(df2, 'PatientID')
+    # print(df.dtypes)
 
-    # print(postSource.generateSample())
-    # log.debug(postSource.defaultSchema)
-
-    # create pipelibe
-    # destination = PostgreSQLDest('My dest', 'import', connection)
-
-    # pg = PostgreSQLConnection.objects(host='localhost').first()
-    # pg.connect()
-
-    # pipeline = Pipeline('Move pipeline')
-
-    # pipeline.addSource(csvSource)
-    # pipeline.setDestination(destination)
-    # pipeline.run()
-
+    # df3 = pd.read_csv('file-storage/mock.csv')
+    # print(df3.dtypes)
+    a = {"res" : {"operator" : "gt", "value": 50}}
+    print(a.res)
+    
 
 if __name__ == '__main__':
     main()
