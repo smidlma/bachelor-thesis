f16c6bd3e98940533dc9960b598bc183e68c5ba3
Martin Šmídl <snidl.martin1@gmail.com>
2022-03-12 18:16:06 +0100

    add componet test

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,5 +9,8 @@ module.exports = {
     rules: {
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
+    },
+    global:{
+      defineProps: 'readonly',
     }
   }
---
+++ frontend/src/components/SSource/SSource.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: 'Name'
+    }
+})
+</script>
+
+<template>
+  <div>Source comp</div>
+</template>  
+
+    
+<style>
+</style>
